e161aaf7ded6cfcab87dab4d67f9cc6b9a228dca
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-09-25 22:45:18 +0300

    chore: Do not use typescript and eslint

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    "./node_modules/real-config/eslint",
-    "./node_modules/real-config/eslint/special-locations",
-    "./.eslintrc.overrides",
-    "./.eslintrc.rule-overrides",
-    "./.eslintrc.temp"
-  ]
-};
